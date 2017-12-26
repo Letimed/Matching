c3d4f13a44a81fe559e24f50664dddbad0452dc2
Tek Florian <camus.florian@gmail.com>
2017-12-26 15:24:47 +0100

    push

--- matchmaking.py
+++ matchmaking.py
@@ -1,10 +1,8 @@
-import csv
 from random import shuffle
 
-pseudo=[]
 nbwin=[str(i) for i in range(1,2000)]
 nblose=[str(i) for i in range(1,2000)]
-nbtotalgame=[nblose + nbwin]
+nbtotalgame=nblose + nbwin
 tempsmoyennesparties=[str(i) for i in range(15,70)]
 kill=[str(i) for i in range(1,70)]
 death=[str(i) for i in range(1,70)]
@@ -14,11 +12,11 @@ report=[str(i) for i in range(1,2000)]
 gametime=[str(i) for i in range(1,100000)]
 ratevictory=[str(i) for i in range(1,100)]
 pctafk=[str(i) for i in range(1,100)]
+nationalité=[str(i) for i in range(1,10)]
 
-def main():
-    matrix = [nw+";"+nl+";"+ntg+";"+tmp+";"+k+";"+d+";"+a+";"+h+";"+r+";"+gt+";"+rv+";"+pca+"\n" for nw in nbwin for nl in nblose for ntg in nbtotalgame for tmp in tempsmoyennesparties for k in kill for d in death for a in assist for h in honor for r in report for gt in gametime for rv in ratevictory for pca in pcafk]
-    shuffle(matrix)
-    f = open("data.csv","w")
-    for x in matrix[:25]:
-        f.write(x)
-    f.close()
+matrix = [nw+";"+nl+";"+nbtotalgame+";"+tmp+";"+k+";"+d+";"+a+";"+h+";"+r+";"+gt+";"+rv+";"+pca+";"+na+"\n" for nw in nbwin for nl in nblose for tmp in tempsmoyennesparties for k in kill for d in death for a in assist for h in honor for r in report for gt in gametime for rv in ratevictory for pca in pctafk for na in nationalité]
+shuffle(matrix)
+f = open("data.csv","w")
+for x in matrix[:25]:
+    f.write(x)
+f.close()
