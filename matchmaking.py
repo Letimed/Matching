81b38e1b3e59baa78bc8f4c1e7e0c6847f2cfec6
Vieirakuh <32671007+Vieirakuh@users.noreply.github.com>
2017-12-26 15:59:54 +0100

    data generator

--- matchmaking.py
+++ matchmaking.py
@@ -1,22 +1,23 @@
-from random import shuffle
+from random import randint
 
-nbwin=[str(i) for i in range(1,2000)]
-nblose=[str(i) for i in range(1,2000)]
-nbtotalgame=nblose + nbwin
-tempsmoyennesparties=[str(i) for i in range(15,70)]
-kill=[str(i) for i in range(1,70)]
-death=[str(i) for i in range(1,70)]
-assist=[str(i) for i in range(1,70)]
-honor=[str(i) for i in range(1,2000)]
-report=[str(i) for i in range(1,2000)]
-gametime=[str(i) for i in range(1,100000)]
-ratevictory=[str(i) for i in range(1,100)]
-pctafk=[str(i) for i in range(1,100)]
-nationalité=[str(i) for i in range(1,10)]
 
-matrix = [nw+";"+nl+";"+nbtotalgame+";"+tmp+";"+k+";"+d+";"+a+";"+h+";"+r+";"+gt+";"+rv+";"+pca+";"+na+"\n" for nw in nbwin for nl in nblose for tmp in tempsmoyennesparties for k in kill for d in death for a in assist for h in honor for r in report for gt in gametime for rv in ratevictory for pca in pctafk for na in nationalité]
-shuffle(matrix)
 f = open("data.csv","w")
-for x in matrix[:25]:
-    f.write(x)
+a = 0
+while a < 200:
+	nbwin=randint(0,2000)
+	nblose=randint(0,2000)
+	nbtotalgame=nblose + nbwin
+	tempsmoyennesparties=randint(15,70)
+	kill=randint(0,70)
+	death=randint(0,70)
+	assist=randint(0,70)
+	honor=randint(0,2000)
+	report=randint(0,2000)
+	gametime=randint(0,100000)
+	ratevictory=randint(0,100)
+	pctafk=randint(0,100)
+	nationalité=randint(1,10)
+	
+	f.write(str(nbwin)+';'+str(nblose)+';'+str(nbtotalgame)+';'+str(tempsmoyennesparties)+';'+str(kill)+';'+str(death)+';'+str(assist)+';'+str(honor)+';'+str(report)+';'+str(gametime)+';'+str(ratevictory)+';'+str(pctafk)+';'+str(nationalité)+'\n')
+	a = a + 1
 f.close()
