2d7c58918c7886fca4ff03465e870f4d4d03f0f9
Vieirakuh <32671007+Vieirakuh@users.noreply.github.com>
2017-12-27 23:34:03 +0100

    backup player class

---
+++ joueurs.py
@@ -0,0 +1,23 @@
+
+
+class Joueur(object):
+
+    def __init__(self, nbwin, nblose, nbtotalgame, tempsmoyennesparties, kill, death, assist, honor, report, gametime, ratevictory, pctafk, nationalité):
+    	self._nbwin = nbwin
+    	self._nblose = nblose
+    	self._nbtotalgame = nbtotalgame
+    	self._tempsmoyenneparties = tempsmoyennesparties
+    	self._kill = kill
+    	self._death = death
+    	self._assist = assist
+    	self._honor = honor
+    	self._report = report
+    	self._gametime = gametime
+    	self._ratevictory = ratevictory
+    	self._pctafk = pctafk
+    	self._nationalité = nationalité
+
+
+    def print(self):
+    	print("Player : " + str(self._nbwin)+';'+str(self._nblose)+';'+str(self._nbtotalgame)+';'+str(self._tempsmoyenneparties)+';'+str(self._kill)+';'+str(self._death)+';'+str(self._assist)+';'+str(self._honor)+';'+str(self._report)+';'+str(self._gametime)+';'+str(self._ratevictory)+';'+str(self._pctafk)+';'+str(self._nationalité)+'\n')
+
---
+++ matchmaking.py
@@ -0,0 +1,4 @@
+from joueurs import Joueur
+
+player = Joueur(1615,1827,3442,66,12,61,50,810,264,24879,40,87,5)
+player.print()
